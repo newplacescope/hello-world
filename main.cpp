a329feb16682a53203b8bfa6998d4dc835bdc05f
newplacescope <31228552+newplacescope@users.noreply.github.com>
2024-10-22 15:55:53 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  cout << "main begin..." << endl;
+  int var1 = 10;
+  int var2 = 12;
+  cout << "main exit..." << endl;
+  
+  return 0;
+}
